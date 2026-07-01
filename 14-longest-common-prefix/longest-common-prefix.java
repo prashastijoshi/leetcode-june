09bf76ad3772b3b094c66391952407f93e8e7286
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-01 23:40:22 +0530

    Time: 4 ms (13.27%) | Memory: 43.5 MB (8.41%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,38 +1,23 @@
-// class Solution {
-//     public String longestCommonPrefix(String[] strs) {
-//         String result="";
-
-//         Arrays.sort(strs);
-//         String w1 = strs[0];
-//         String w2 = strs[strs.length-1];
-//         if(w1.length()==0 || w2.length()==0)return result;
-//         // int i = 0;
-//         for(int i = 0;i<Math.min(w1.length(),w2.length()); i++){
-//             if(w1.charAt(i) != w2.charAt(i)){
-//                 return result;
-//             }
-//             result = result +w1.charAt(i);
-        
-//             // i++;
-//         }
-
-
-//         return result;
-        
-//     }
-// }
 class Solution {
-    public String longestCommonPrefix(String[] v) {
-        StringBuilder ans = new StringBuilder();
-        Arrays.sort(v);
-        String first = v[0];
-        String last = v[v.length-1];
-        for (int i=0; i<Math.min(first.length(), last.length()); i++) {
-            if (first.charAt(i) != last.charAt(i)) {
-                return ans.toString();
+    public String longestCommonPrefix(String[] strs) {
+        String result="";
+
+        Arrays.sort(strs);
+        String w1 = strs[0];
+        String w2 = strs[strs.length-1];
+        // if(w1.length()==0 || w2.length()==0)return result;
+        // int i = 0;
+        for(int i = 0;i<Math.min(w1.length(),w2.length()); i++){
+            if(w1.charAt(i) != w2.charAt(i)){
+                return result;
             }
-            ans.append(first.charAt(i));
+            result = result +w1.charAt(i);
+        
+            // i++;
         }
-        return ans.toString();
+
+
+        return result;
+        
     }
 }

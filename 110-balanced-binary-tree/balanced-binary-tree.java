55c9422b6d7c82e524222e5b04bbae1d56ce897b
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-02 14:49:58 +0530

    Time: 1 ms (31.81%) | Memory: 46.3 MB (10.14%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,40 +1,3 @@
-// /**
-//  * Definition for a binary tree node.
-//  * public class TreeNode {
-//  *     int val;
-//  *     TreeNode left;
-//  *     TreeNode right;
-//  *     TreeNode() {}
-//  *     TreeNode(int val) { this.val = val; }
-//  *     TreeNode(int val, TreeNode left, TreeNode right) {
-//  *         this.val = val;
-//  *         this.left = left;
-//  *         this.right = right;
-//  *     }
-//  * }
-//  */
-// class Solution {
-//     public boolean isBalanced(TreeNode root) {
-//         return dfs(root)[0] == 1;
-//     }
-//     public int[] dfs(TreeNode root){
-//         if(root==null){
-//             return new int[] {1,0};
-//         }
-//         int[] left = dfs(root.left);
-//         int[] right = dfs(root.right);
-
-//         int height = Math.max(left[1],right[1])+1;
-
-//         boolean balanced = (Math.abs(left[1]-right[1])<=1 && left[0] == 1 && right[0] == 1);
-          
-        
-        
-//         return new int[]{balanced?1:0,height};
-        
-//     }
-// }
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -50,32 +13,24 @@
  *     }
  * }
  */
-
-public class Solution {
+class Solution {
     public boolean isBalanced(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode node = root, last = null;
-        Map<TreeNode, Integer> depths = new HashMap<>();
-
-        while (!stack.isEmpty() || node != null) {
-            if (node != null) {
-                stack.push(node);
-                node = node.left;
-            } else {
-                node = stack.peek();
-                if (node.right == null || last == node.right) {
-                    stack.pop();
-                    int left = depths.getOrDefault(node.left, 0);
-                    int right = depths.getOrDefault(node.right, 0);
-                    if (Math.abs(left - right) > 1) return false;
-                    depths.put(node, 1 + Math.max(left, right));
-                    last = node;
-                    node = null;
-                } else {
-                    node = node.right;
-                }
-            }
+        return dfs(root)[0] == 1;
+    }
+    public int[] dfs(TreeNode root){
+        if(root==null){
+            return new int[] {1,0};
         }
-        return true;
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+
+        int height = Math.max(left[1],right[1])+1;
+
+        boolean balanced = (Math.abs(left[1]-right[1])<=1 && left[0] == 1 && right[0] == 1);
+          
+        
+        
+        return new int[]{balanced?1:0,height};
+        
     }
 }

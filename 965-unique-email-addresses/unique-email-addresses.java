468f595d69b28b128e8d86ca69aab8b3e7d07c0f
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-04 13:33:19 +0530

    Time: 16 ms (19.05%) | Memory: 46.6 MB (47.77%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.java
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -1,18 +1,41 @@
-class Solution {
+// class Solution {
+//     public int numUniqueEmails(String[] emails) {
+//         Set<String> unique = new HashSet<>();
+//         for(int i = 0; i<emails.length; i++){
+//             String[] sep = emails[i].split("@");
+//             String local = sep[0];
+//             String domain = sep[1];
+
+//             local = local.split("\\+")[0];
+//             local = local.replace(".","");
+
+//             unique.add(local+"@"+domain);
+//         }
+//         return unique.size();
+        
+        
+//     }
+// }
+public class Solution {
     public int numUniqueEmails(String[] emails) {
         Set<String> unique = new HashSet<>();
-        for(int i = 0; i<emails.length; i++){
-            String[] sep = emails[i].split("@");
-            String local = sep[0];
-            String domain = sep[1];
 
-            local = local.split("\\+")[0];
-            local = local.replace(".","");
+        for (String e : emails) {
+            int i = 0;
+            StringBuilder local = new StringBuilder();
+            while (i < e.length() && e.charAt(i) != '@' && e.charAt(i) != '+') {
+                if (e.charAt(i) != '.') {
+                    local.append(e.charAt(i));
+                }
+                i++;
+            }
 
-            unique.add(local+"@"+domain);
+            while (i < e.length() && e.charAt(i) != '@') {
+                i++;
+            }
+            String domain = e.substring(i + 1);
+            unique.add(local.toString() + "@" + domain);
         }
         return unique.size();
-        
-        
     }
 }

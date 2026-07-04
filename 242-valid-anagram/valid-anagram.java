0437e6b5796a46c904f7cc63b082dbc62406b999
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-04 08:30:37 +0530

    Time: 27 ms (5.26%) | Memory: 46.4 MB (41.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,16 +1,31 @@
-class Solution {
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length()!=t.length()){
+//             return false;
+//         }
+//         Map<Character,Integer> sm = new HashMap<>();
+//         Map<Character,Integer> tm = new HashMap<>();
+
+//         for(int i = 0; i<s.length(); i++){
+//             sm.put(s.charAt(i),sm.getOrDefault(s.charAt(i),0)+1);
+//             tm.put(t.charAt(i),tm.getOrDefault(t.charAt(i),0)+1);
+//         }
+//         return(sm.equals(tm));
+        
+//     }
+// }
+public class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
+        if (s.length() != t.length()) {
             return false;
         }
-        Map<Character,Integer> sm = new HashMap<>();
-        Map<Character,Integer> tm = new HashMap<>();
 
-        for(int i = 0; i<s.length(); i++){
-            sm.put(s.charAt(i),sm.getOrDefault(s.charAt(i),0)+1);
-            tm.put(t.charAt(i),tm.getOrDefault(t.charAt(i),0)+1);
+        HashMap<Character, Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
         }
-        return(sm.equals(tm));
-        
+        return countS.equals(countT);
     }
 }

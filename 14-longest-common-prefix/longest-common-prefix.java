fece3b8502e78b54aa4d9b6e2d47d7b58ca34be7
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-04 09:46:13 +0530

    Time: 1 ms (62.62%) | Memory: 43.3 MB (32.65%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,18 +3,18 @@ class Solution {
         if(strs.length==1){
             return strs[0];
         }
-        StringBuilder sb = new StringBuilder();
+        // StringBuilder sb = new StringBuilder();
         Arrays.sort(strs);
         String w1 = strs[0];
         String w2 = strs[strs.length-1];
        
         for(int i = 0; i<Math.min(w1.length(),w2.length());i++){
             if(w1.charAt(i)!=w2.charAt(i)){
-                return sb.toString();
+                return strs[0].substring(0,i);
             }
-            sb.append(w2.charAt(i));
+           
         }
         
-        return sb.toString();
+        return strs[0];
     }
 }

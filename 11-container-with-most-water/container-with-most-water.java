53b01b9c3594f0bbbf9a4a849d8b3397d4da98c0
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-05 12:57:18 +0530

    Time: 5 ms (82.38%) | Memory: 77.1 MB (84.90%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -7,18 +7,19 @@ class Solution {
             int currArea = Math.min(height[i], height[j]) * (j - i);
             maxarea = Math.max(maxarea, currArea);
 
-            if (height[i] < height[j]) {
+            if (height[i] <= height[j]) {
                 i++;
-            } else if (height[i] > height[j]) {
-                j--;
             } else {
-                if (height[i + 1] < height[j - 1]) {
-                    j--;
-                } else {
-                    i++;
-                }
-
+                j--;
             }
+            // } else {
+            //     if (height[i + 1] < height[j - 1]) {
+            //         j--;
+            //     } else {
+            //         i++;
+            //     }
+
+            // }
 
         }
         return maxarea;

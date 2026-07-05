e325ca9b0198dff3f573f1619149e7852395cdce
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-05 14:30:39 +0530

    Time: 2 ms (47.22%) | Memory: 44 MB (42.51%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> stack = new Stack<>();
+        for(String str:logs){
+            if(str.equals("../")){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }
+            else if(str.equals("./")){
+                continue;
+            }
+            else{
+                stack.push(str);
+            }
+        }
+
+        int count = 0;
+        while(!stack.isEmpty()){
+            stack.pop();
+            count++;
+        }
+        return count;
+    }
+}

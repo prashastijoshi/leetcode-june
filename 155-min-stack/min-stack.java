b3f3d5aab17735aa9787130b639d90820f0cdc6c
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-05 14:53:32 +0530

    Time: 35 ms (25.99%) | Memory: 100.1 MB (27.62%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,45 +1,78 @@
-class MinStack {
+// class MinStack {
+//     private Stack<Integer> stack;
+//     private Stack<Integer> minStack;
+
+//     public MinStack() {
+//         stack = new Stack<>();
+//         minStack = new Stack<>();
+        
+//     }
+    
+//     public void push(int value) {
+//         stack.push(value);
+//         if(minStack.isEmpty() || value<=minStack.peek()){
+//             minStack.push(value);
+//         }
+        
+//     }
+    
+//     public void pop() {
+//         if(stack.isEmpty())return;
+//         int top = stack.pop();
+//         if(top==minStack.peek()) minStack.pop();
+        
+//     }
+    
+//     public int top() {
+//         return stack.peek();
+        
+//     }
+    
+//     public int getMin() {
+//         // if(!stack.isEmpty())
+//         return minStack.peek();
+        
+//     }
+// }
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack obj = new MinStack();
+//  * obj.push(value);
+//  * obj.pop();
+//  * int param_3 = obj.top();
+//  * int param_4 = obj.getMin();
+//  */
+
+public class MinStack {
     private Stack<Integer> stack;
     private Stack<Integer> minStack;
 
     public MinStack() {
         stack = new Stack<>();
         minStack = new Stack<>();
-        
     }
-    
-    public void push(int value) {
-        stack.push(value);
-        if(minStack.isEmpty() || value<=minStack.peek()){
-            minStack.push(value);
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
         }
-        
     }
-    
+
     public void pop() {
-        if(stack.isEmpty())return;
+        if (stack.isEmpty()) return;
         int top = stack.pop();
-        if(top==minStack.peek()) minStack.pop();
-        
+        if (top == minStack.peek()) {
+            minStack.pop();
+        }
     }
-    
+
     public int top() {
         return stack.peek();
-        
     }
-    
+
     public int getMin() {
-        // if(!stack.isEmpty())
         return minStack.peek();
-        
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(value);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */

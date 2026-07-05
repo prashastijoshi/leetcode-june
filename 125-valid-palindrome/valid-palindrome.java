1ddcb8900236d36f0f8184f42d57492b33db76ca
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-05 09:13:41 +0530

    Time: 2 ms (99.26%) | Memory: 44.5 MB (57.42%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,17 +1,42 @@
-class Solution {
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         String check = s.toLowerCase().trim().replaceAll("[^a-z0-9]","");
+//         System.out.println(check);
+//         int i = 0;
+//         int j = check.length()-1;
+//         while(i<j){
+//             if(check.charAt(i)!=check.charAt(j)){
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+//         return true;
+        
+//     }
+// }
+public class Solution {
     public boolean isPalindrome(String s) {
-        String check = s.toLowerCase().trim().replaceAll("[^a-z0-9]","");
-        System.out.println(check);
-        int i = 0;
-        int j = check.length()-1;
-        while(i<j){
-            if(check.charAt(i)!=check.charAt(j)){
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !alphaNum(s.charAt(l))) {
+                l++;
+            }
+            while (r > l && !alphaNum(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
                 return false;
             }
-            i++;
-            j--;
+            l++; r--;
         }
         return true;
-        
+    }
+
+    public boolean alphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9');
     }
 }

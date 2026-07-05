4273798d511f378a4831d3df1bd87ad5df72ce9a
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-05 09:29:59 +0530

    Time: 98 ms (6.81%) | Memory: 47.7 MB (46.72%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -15,26 +15,18 @@ class Solution {
         
     }
     public boolean isPalindrome(String s) {
-        int l = 0, r = s.length() - 1;
-
-        while (l < r) {
-            while (l < r && !alphaNum(s.charAt(l))) {
-                l++;
-            }
-            while (r > l && !alphaNum(s.charAt(r))) {
-                r--;
-            }
-            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+        String check = s.toLowerCase().trim().replaceAll("[^a-z0-9]","");
+        System.out.println(check);
+        int i = 0;
+        int j = check.length()-1;
+        while(i<j){
+            if(check.charAt(i)!=check.charAt(j)){
                 return false;
             }
-            l++; r--;
+            i++;
+            j--;
         }
         return true;
-    }
-
-    public boolean alphaNum(char c) {
-        return (c >= 'A' && c <= 'Z' ||
-                c >= 'a' && c <= 'z' ||
-                c >= '0' && c <= '9');
+        
     }
 }

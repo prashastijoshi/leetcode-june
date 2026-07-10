e50c7570738da70bd4d0713d8a8992ddb58acd73
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-10 15:44:52 +0530

    Time: 3 ms (91.20%) | Memory: 46.7 MB (59.42%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,30 +1,27 @@
 class Solution {
+    private List<List<Integer>> res = new ArrayList<>();
+    private List<Integer> subset = new ArrayList<>();
+    private boolean[] picked ;
     public List<List<Integer>> permuteUnique(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> subset = new ArrayList<>();
-        boolean[] picked = new boolean[nums.length];
-        backtrack(nums,subset,res,picked);
+        picked = new boolean[nums.length];
+        backtrack(nums,picked);
         return res;
-        
     }
-    public void backtrack(int[] nums, List<Integer> subset,List<List<Integer>> res, boolean[] picked){
-        if(nums.length==subset.size()){
+    public void backtrack(int[] nums, boolean[] picked){
+        if(subset.size()==nums.length){
             res.add(new ArrayList<>(subset));
             return;
         }
-        for(int i =0;i<nums.length;i++){
-            if(picked[i] || (i>0 && nums[i] == nums[i-1] && !picked[i-1]) ){
+        for(int i = 0;i<nums.length;i++){
+            if(picked[i] || (i>0 && nums[i] == nums[i-1] && !picked[i-1])){
                 continue;
             }
-       
-            picked[i] =true;
+            picked[i] = true;
             subset.add(nums[i]);
-            backtrack(nums,subset,res,picked); 
+            backtrack(nums,picked);
+            picked[i] = false;
             subset.remove(subset.size()-1);
-            picked[i]=false; 
-
-            
             
         }
         return;

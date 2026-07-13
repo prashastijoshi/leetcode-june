c4352c15f14899724d646c3e835dde3471454ff8
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-13 14:43:37 +0530

    Time: 1 ms (99.94%) | Memory: 48 MB (43.16%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,23 +1,37 @@
-class Solution {
-    public boolean canJump(int[] nums) {
-        int n = nums.length;
-        boolean[] dp = new boolean[n+1];
-        if(nums[0] ==0 && nums.length>1)return false;
-        else if(nums.length==1) return true;
-        dp[0] = true;
-        // dp[1] = true;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(dp[j] && j+nums[j]>=i){
-                    dp[i] = true;
-                    break;
-                }
-                // dp[i] =false;
-            }
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//         int n = nums.length;
+//         boolean[] dp = new boolean[n+1];
+//         if(nums[0] ==0 && nums.length>1)return false;
+//         else if(nums.length==1) return true;
+//         dp[0] = true;
+//         // dp[1] = true;
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<i;j++){
+//                 if(dp[j] && j+nums[j]>=i){
+//                     dp[i] = true;
+//                     break;
+//                 }
+//                 // dp[i] =false;
+//             }
             
-        }  
-        return dp[n-1];
+//         }  
+//         return dp[n-1];
 
         
+//     }
+// }
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
     }
 }

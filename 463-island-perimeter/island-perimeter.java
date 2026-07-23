5728b66f92c913bba12c5a3debb5d526189fb0de
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-23 10:37:53 +0530

    Time: 8 ms (17.11%) | Memory: 47 MB (82.72%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,34 +1,71 @@
-class Solution {
+// class Solution {
+//     private int[][] grid;
+//     private int rows, cols;
+//     private boolean[][] visited;
+//     public int islandPerimeter(int[][] grid) {
+//         this.grid = grid;
+//         this.rows = grid.length;
+//         this.cols = grid[0].length;
+//         this.visited = new boolean[rows][cols];
+//         int perimeter=0;
+
+//         for(int i=0;i<rows;i++){
+//             for(int j=0;j<cols;j++){
+//                 if(grid[i][j]==1){
+//                     return dfs(i,j);
+//                 }
+//             }
+//         }
+//         return 0;
+
+        
+//     }
+//     public int dfs(int i,int j){
+//         if(i<0 || j<0 || i>=rows || j>=cols || grid[i][j]==0){
+//             return 1;
+//         }
+//         if(visited[i][j]){
+//             return 0;
+//         }
+
+//         visited[i][j] = true;
+//         return dfs(i,j+1)+dfs(i+1,j)+dfs(i-1,j)+dfs(i,j-1);
+//     }
+// }
+
+
+public class Solution {
     private int[][] grid;
-    private int rows, cols;
     private boolean[][] visited;
+    private int rows, cols;
+
     public int islandPerimeter(int[][] grid) {
         this.grid = grid;
         this.rows = grid.length;
         this.cols = grid[0].length;
         this.visited = new boolean[rows][cols];
-        int perimeter=0;
 
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==1){
-                    return dfs(i,j);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    return dfs(i, j);
                 }
             }
         }
         return 0;
-
-        
     }
-    public int dfs(int i,int j){
-        if(i<0 || j<0 || i>=rows || j>=cols || grid[i][j]==0){
+
+    private int dfs(int i, int j) {
+        if (i < 0 || j < 0 || i >= rows ||
+            j >= cols || grid[i][j] == 0) {
             return 1;
         }
-        if(visited[i][j]){
+        if (visited[i][j]) {
             return 0;
         }
 
         visited[i][j] = true;
-        return dfs(i,j+1)+dfs(i+1,j)+dfs(i-1,j)+dfs(i,j-1);
+        return dfs(i, j + 1) + dfs(i + 1, j) +
+               dfs(i, j - 1) + dfs(i - 1, j);
     }
 }

c6d0c9d44ac9a4e7610fed889e8f3ffbce6cc66d
prashastijoshi <prashasti.joshi1008@gmail.com>
2026-07-25 12:11:21 +0530

    Time: 5 ms (28.49%) | Memory: 51.3 MB (89.12%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,45 +1,86 @@
-class Solution {
-    private static final int[][] options = {{1,0},{0,1},{-1,0},{0,-1}};
-    public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int islands=0;
+// class Solution {
+//     private static final int[][] options = {{1,0},{0,1},{-1,0},{0,-1}};
+//     public int numIslands(char[][] grid) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         int islands=0;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j] == '1'){
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(grid[i][j] == '1'){
+//                     islands++;
+//                     bfs(grid, i,j , m,n);
+//                 }
+//             }
+//         }
+//         return islands;
+        
+//     }
+
+//     public void bfs(char[][] grid, int r, int c,int m , int n){
+//         Queue<int[]> q = new LinkedList<>();
+//         grid[r][c] = '0';
+//         q.add(new int[]{r,c});
+
+//         while(!q.isEmpty()){
+//             int[] node = q.poll();
+//             int row = node[0];
+//             int col = node[1];
+
+
+
+//             for(int[] direction:options){
+//                 int nr = row+direction[0];
+//                 int nc = col+direction[1];
+
+//                 if(nr>=0 && nc >=0 && nr<m && nc <n && grid[nr][nc] == '1'){
+//                     q.add(new int[]{nr,nc});
+//                     grid[nr][nc] = '0';
+//                 }
+//             }
+
+
+//         }
+//     }
+// }
+
+public class Solution {
+    private static final int[][] directions = {{1, 0}, {-1, 0},
+                                               {0, 1}, {0, -1}};
+
+    public int numIslands(char[][] grid) {
+        int ROWS = grid.length, COLS = grid[0].length;
+        int islands = 0;
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (grid[r][c] == '1') {
+                    bfs(grid, r, c);
                     islands++;
-                    bfs(grid, i,j);
                 }
             }
         }
+
         return islands;
-        
     }
 
-    public void bfs(char[][] grid, int r, int c){
+    private void bfs(char[][] grid, int r, int c) {
         Queue<int[]> q = new LinkedList<>();
         grid[r][c] = '0';
-        q.add(new int[]{r,c});
+        q.add(new int[]{r, c});
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int[] node = q.poll();
-            int row = node[0];
-            int col = node[1];
+            int row = node[0], col = node[1];
 
-
-
-            for(int[] direction:options){
-                int nr = row+direction[0];
-                int nc = col+direction[1];
-
-                if(nr>=0 && nc >=0 && nr<grid.length && nc <grid[0].length && grid[nr][nc] == '1'){
-                    q.add(new int[]{nr,nc});
+            for (int[] dir : directions) {
+                int nr = row + dir[0], nc = col + dir[1];
+                if (nr >= 0 && nc >= 0 && nr < grid.length &&
+                    nc < grid[0].length && grid[nr][nc] == '1') {
+                    q.add(new int[]{nr, nc});
                     grid[nr][nc] = '0';
                 }
             }
-
-
         }
     }
 }
